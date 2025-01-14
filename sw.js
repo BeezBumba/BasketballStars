7ffa4746f3232fc41480d6a18c22e6c7b432dbc3
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2025-01-13 22:07:39 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,38 @@
-const CACHE_NAME = 'basketball-stars';
-const urlsToCache = [
-  '/',
-  '/assets',
-  '/index.html',
-  '/script.js',
-  '/manifest.json',
-  '/basketball_legends_2019.min.js',
-  '/basketball-stars.png'
-];
+const KEY = 'BASKETBALLSTARS';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (event) => {
+    event.waitUntil(self.skipWaiting());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request)
-          .then(fetchResponse => {
-            return caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, fetchResponse.clone());
-                return fetchResponse;
-              });
-          });
-      })
+self.addEventListener('message', (event) => {
+    if (event.data.type === 'CACHE_URLS') {
+        event.waitUntil(
+            caches.open(KEY)
+                .then( (cache) => {
+                    return cache.addAll(event.data.payload);
+                })
+        );
+    }
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      try {
+        console.log(`[Service Worker] Attempting to serve resource from cache: ${e.request.url}`);
+        const r = await caches.match(e.request);
+        if (r) {
+          return r;
+        }
+        console.log(`[Service Worker] Attempting live fetch: ${e.request.url}`);
+        const response = await fetch(e.request);
+        const cache = await caches.open(KEY);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+      } catch (err) {
+        console.error(`[Service Worker] Fetch failed: ${err}`);
+      }
+    })()
   );
 });
